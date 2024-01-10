f4881ffd76dc7d9c10bc5a3e9ecf2fe73c59d123
huylaiphu3 <phuhuytapcode@gmail.com>
2024-01-11 00:35:51 +0700

    commit on linux

--- src/main/java/com/example/demo1/controller/RoomController.java
+++ src/main/java/com/example/demo1/controller/RoomController.java
@@ -1,5 +1,21 @@
 package com.example.demo1.controller;
 
+import com.example.demo1.model.Room;
+import com.example.demo1.response.RoomResponse;
+import com.example.demo1.service.IRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+@RequiredArgsConstructor
 public class RoomController {
-    
+    private final IRoomService roomService;
+    public ResponseEntity<RoomResponse> addNewRoom(@RequestParam("photo") MultipartFile photo,
+                                                   @RequestParam("roomType") String roomType,
+                                                   @RequestParam("roomPrice") BigDecimal roomPrice){
+        Room savedRoom = roomService.addNewRoom(photo,roomType,roomPrice);
+        RoomResponse response = new RoomResponse(savedRoom.getId(),savedRoom.getRoomType(),savedRoom.getRoomPrice());
+    }
 }
