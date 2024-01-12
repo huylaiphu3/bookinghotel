0ed41cb60a451bdd999318297bebdd3e977c4dce
huylaiphu3 <phuhuytapcode@gmail.com>
2024-01-12 09:31:35 +0700

    12-1-20224

--- src/main/java/com/example/demo1/service/BookedRoomService.java
+++ src/main/java/com/example/demo1/service/BookedRoomService.java
@@ -1,5 +1,5 @@
 package com.example.demo1.service;
 
-public class RoomServiceImpl {
+public class BookedRoomService {
     
 }
--- src/main/java/com/example/demo1/service/BookedRoomServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo1.service;
-
-public class BookedRoomServiceImpl{
-    
-}
